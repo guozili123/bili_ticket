6efa32b31560a0d1d0dc943986411177fc05d1ce
yao12345678 <11782276+yao12345678@user.noreply.gitee.com>
2023-07-12 01:19:55 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# 以下为原作者https://github.com/fengx1a0/Bilibili_show_ticket_auto_order 声明
+
+# Bilibili_show_ticket_auto_order
+
+本项目核心借鉴自https://github.com/Hobr 佬
+
+Bilibili会员购抢票助手, 通过B站接口抢购目标漫展/演出
+
+本脚本仅供学习交流使用, 不得用于商业用途, 如有侵权请联系删除
+
+<img src="images\image-20230708221711220.png" alt="image-20230708221711220" style="zoom:50%;" />
+
+<img src="images/a.png" alt="image-20230708221143842" style="zoom:50%;" />
+
+## 功能截图
+
+除了登录纯api请求
+
+目前已经支持漫展演出等的 无证 / 单证 / 一人一证 的购买
+
+<img src="images/image-20230708014050624.png" alt="image-20230708014050624" style="zoom:50%;" />
+
+<img src="images\image-20230708014124395.png" alt="image-20230708014124395" style="zoom:50%;" />
+
+## 使用
+
+### 执行exe
+
+登录和抢票分开的，先运行登录.exe，登陆后再运行抢票.exe，运行了之后不要急着选，先把验证.exe启动起来
+
+不需要依赖
+
+### 执行脚本
+
+```shell
+python login.py
+python main.py
+python geetest.py
+```
+
+该装的东西自己装
+
+## 配置说明
+
+config.txt为配置文件，不指定值为None
+
+- proxies 指定代理 如：127.0.0.1:8080 (IP:PORT 请不要加前缀)
+- specificID 多个用户登陆后指定某一个人uid(bilibili) (多用户还没做等后面有必要再写)
+- sleep设置每次抢票请求间隔时间
+
+## API文档
+
+pass
+
+## 问题报告
+
+提issue即可
+
+## 更新
+
+加入token验证，手动拉滑块
+
+# 以下为本项目附加声明
+
+## 更新
+
+加入假票过滤功能
+加入速度调节功能
---
+++ geetest.py
@@ -0,0 +1,77 @@
+import os
+import json
+import win32api,win32con
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+class dealCode():
+	def __init__(self,specificID=None):
+
+		self.specificID = specificID
+		self.cookies = {}
+		self.u = ""
+
+	def load_cookies(self):
+		if not os.path.exists("user_data.json"):
+			print("未找到用户数据文件")
+			os.system("pause")
+			exit()
+		with open("user_data.json","r") as r:
+			try:
+				data = json.load(r)
+				if len(data) == 0:
+					raise ""
+			except:
+				print("数据文件错误，请登陆后尝试")
+			if self.specificID:
+				data = data[self.specificID][1]
+			else:
+				data = data[list(data.keys())[0]][1]
+			for item in data.split(";"):
+				i = item.split("=")
+				if len(i) == 2:
+					self.WebDriver.add_cookie(cookie_dict={"domain": ".bilibili.com" ,'name' : i[0].strip(), 'value' : i[1].strip()})
+	
+	def init_browser(self):
+		options = webdriver.EdgeOptions()
+		options.add_argument("--log-level=3")
+		self.WebDriver = webdriver.Edge(options=options)
+		self.WebDriver.get("https://bilibili.com")
+		self.WebDriver.delete_all_cookies()
+
+	def load_code(self,url):
+		print("token需要验证！！正在加载验证码，请完成验证后关闭网页！！")
+		self.init_browser()
+		self.load_cookies()
+		self.WebDriver.get(url)
+		while True:
+			sleep(0.1)
+			try:
+				self.WebDriver.execute_script('javascript:void(0);')
+			except:
+				return 1
+
+	def test(self):
+		self.load_code("https://bilibili.com")
+
+	def mult_work(self):
+		self.init_browser()
+		self.load_cookies()
+		while True:
+			sleep(0.25)
+			if not os.path.exists("url"):
+				print("请在脚本目录打开此程序")
+				os.system("pause")
+			a = open("url","r")
+			u = a.read()
+			a.close()
+			if u and u.strip() != self.u:
+				self.u = u
+				win32api.MessageBox(0,'需要滑块验证！','通知',win32con.MB_OK)
+				self.WebDriver.get(u)
+
+if __name__ == '__main__':
+	dealCode().mult_work()
+
+
